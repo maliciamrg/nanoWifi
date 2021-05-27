7fc8529fd0a40b628234bbf5e085b0a690c994f3
MaliciaMrg <maliciamrg@users.noreply.github.com>
2021-05-27 16:00:50 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+SoftwareSerial softSerial(2, 3); // RX, TX
+ 
+void setup() 
+{
+  uint32_t baud = 9600;
+  Serial.begin(baud);
+  softSerial.begin(baud);
+  Serial.print("SETUP!! @");
+  Serial.println(baud);
+}
+ 
+void loop() 
+{
+    while(softSerial.available() > 0) 
+    {
+      char a = softSerial.read();
+      if(a =='\0')
+        continue;
+      if(a != 'r' && a != 'n' && (a < 32))
+        continue;
+      Serial.print(a);
+    }
+    
+    while(Serial.available() > 0)
+    {
+      char a = Serial.read();
+      Serial.write(a);
+      softSerial.write(a);
+    }
+}
